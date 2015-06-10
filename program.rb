d9a6568cf59e607ebd69eb4165be7bc3d68dc8db
Jason Liggi <jason@liggi.net>
2015-06-10 16:29:35 +0100

    Starting on the console-based user interface

--- program.rb
+++ program.rb
@@ -1,4 +1,26 @@
 require_relative 'editor'
 
+def print_system_message(message)
+  puts "---------------"
+  puts message
+  puts "---------------"
+end
+
 editor = BitmapEditor.new(4, 4)
-puts editor.show_table
+exit = false
+
+print_system_message "Hi, and welcome to my Bitmap Editor!"
+
+while(!exit)
+  print "> "
+  command = gets.chomp.downcase
+
+  if(command == "s")
+    puts editor.show_table
+  end
+
+  if(command == "x")
+    exit = true
+    print_system_message "Have a nice day!"
+  end
+end
