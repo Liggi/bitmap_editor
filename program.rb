666cf855f5db4c2d689adec11b34e85c5588f7f1
Jason Liggi <jason@liggi.net>
2015-06-10 16:21:08 +0100

    Improving initial test slightly, creating a separate interface for the program itself

---
+++ program.rb
@@ -0,0 +1,4 @@
+require_relative 'editor'
+
+editor = BitmapEditor.new(4, 4)
+puts editor.show_table
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -1,10 +1,10 @@
 require_relative '../editor'
 
 RSpec.describe BitmapEditor, "#create" do
-  context "a basic 1x4 image" do
+  context "a basic 4x4 image" do
     it "returns an appropriate string" do
-      editor = BitmapEditor.new(1, 4)
-      expect(editor.show_table).to eq "O O O O"
+      editor = BitmapEditor.new(4, 4)
+      expect(editor.show_table).to eq "O O O O\nO O O O\nO O O O\nO O O O\n"
     end
   end
 end
